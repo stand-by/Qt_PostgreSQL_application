b15f8939d6c80deceda82d7b8bccd67012c1e7a2
eugene <eugene.rublenko@gmail.com>
2017-04-26 00:40:30 +0300

    implement addproduct class

--- addproduct.cpp
+++ addproduct.cpp
@@ -1,14 +1,115 @@
 #include "addproduct.h"
 #include "ui_addproduct.h"
 
-addproduct::addproduct(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::addproduct)
-{
+addproduct::addproduct(QWidget *parent, QSqlDatabase db_): QDialog(parent), ui(new Ui::addproduct), insert_new_type(false),is_filled(false){
     ui->setupUi(this);
+    db = db_;
+
+    //hide parts that necessary for adding new type
+    ui->input_new_type->hide();
+    ui->label_new_type->hide();
+
+    //initial window config
+    this->setModal(true);
+    this->setWindowTitle("Додати новий товар");
+
+    //query two columns of one table into two different models
+    model_names = new QSqlQueryModel(this);
+    model_names->setQuery("SELECT name_of_type FROM type_of_goods ORDER BY id_type_of_goods;", db);
+    model_ids = new QSqlQueryModel(this);
+    model_ids->setQuery("SELECT id_type_of_goods FROM type_of_goods ORDER BY id_type_of_goods;", db);
+
+    //set some initial value to combobox
+    ui->box_good_type->setModel(model_names);
+    ui->box_good_type->setCurrentIndex(0);
+    ui->box_good_type->view()->setCurrentIndex(model_names->index(0,0));
+
+    //for debug purposes
+    //for(int i = 0; i < model_names->rowCount(); ++i)
+    //    qDebug() << model_ids->record(i).value(0).toString() << model_names->record(i).value(0).toString();
 }
 
-addproduct::~addproduct()
-{
+addproduct::~addproduct() {
+    delete model_names;
+    delete model_ids;
     delete ui;
+}
+
+bool addproduct::is_valid() {
+    return is_filled;
+}
+
+//hide or show additional fields
+void addproduct::on_button_add_type_clicked() {
+    switch_modes();
+}
+
+//change UI to give ability to add new goods type
+void addproduct::switch_modes() {
+    insert_new_type = !insert_new_type;
+    if(insert_new_type) {
+        ui->input_new_type->show();
+        ui->label_new_type->show();
+        ui->box_good_type->setEnabled(false);
+        ui->button_add_type->setText("-");
+    } else {
+        ui->input_new_type->hide();
+        ui->label_new_type->hide();
+        ui->box_good_type->setEnabled(true);
+        ui->button_add_type->setText("+");
+    }
+}
+
+//add new goods type to db and update type_id which will be used later in product insertion
+void addproduct::perform_type_insert() {
+    QString query = QString("SELECT add_type_of_goods('%1')").arg(ui->input_new_type->text());
+    QSqlQuery qs = db.exec(query);
+
+    if(db.lastError().isValid()) {
+        qDebug() << db.lastError();
+        prompt_error("Додавання нового типу не вiдбулося");
+        return;
+    }
+
+    qs.first();
+    type_id = qs.value(0).toInt();
+}
+
+//add new product to goods table with appropriate type_id
+void addproduct::perform_product_insert() {
+    qDebug() << "type_id =" << type_id;
+
+    QString query = QString("SELECT add_goods('%1','%2'::int)").arg(ui->input_good_name->text(),QString::number(type_id));
+    db.exec(query);
+    if(db.lastError().isValid()) {
+        qDebug() << db.lastError();
+        prompt_error("Додавання нового товару не вiдбулося");
+    }
+}
+
+void addproduct::on_button_box_accepted() {
+    if(insert_new_type && ui->input_new_type->text()=="") {
+        prompt_error("Ви залишили пустi поля. Заповнiть форму знову!");
+        return;
+    }
+    if(ui->input_good_name->text()=="") {
+        prompt_error("Ви залишили пустi поля. Заповнiть форму знову!");
+        return;
+    }
+
+    type_id = model_ids->data(ui->box_good_type->view()->currentIndex()).toInt();
+    if(insert_new_type) perform_type_insert();
+    perform_product_insert();
+
+    is_filled = true;
+}
+
+void addproduct::prompt_error(QString text, bool exit_flag) {
+    QMessageBox messageBox;
+    messageBox.critical(0,"Помилка",text);
+    messageBox.setFixedSize(500,200);
+    if(exit_flag) {
+        QApplication::closeAllWindows();
+        exit(0);
+    }
 }
--- addproduct.h
+++ addproduct.h
@@ -2,6 +2,10 @@
 #define ADDPRODUCT_H
 
 #include <QDialog>
+#include <QtSql>
+#include <QSqlQueryModel>
+#include <QDesktopWidget>
+#include <QMessageBox>
 
 namespace Ui {
 class addproduct;
@@ -12,11 +16,29 @@ class addproduct : public QDialog
     Q_OBJECT
 
 public:
-    explicit addproduct(QWidget *parent = 0);
+    explicit addproduct(QWidget *parent, QSqlDatabase db);
     ~addproduct();
+    bool is_valid();
+
+private slots:
+    void on_button_box_accepted();
+    void on_button_add_type_clicked();
 
 private:
     Ui::addproduct *ui;
+
+    bool insert_new_type;
+    bool is_filled;
+    int type_id;
+
+    QSqlQueryModel *model_names;
+    QSqlQueryModel *model_ids;
+    QSqlDatabase db;
+
+    void prompt_error(QString text, bool exit_flag = false);
+    void switch_modes();
+    void perform_type_insert();
+    void perform_product_insert();
 };
 
 #endif // ADDPRODUCT_H
