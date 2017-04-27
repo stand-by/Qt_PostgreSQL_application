801f0edba9b6df3051cf854b2a47597497ecb031
eugene <eugene.rublenko@gmail.com>
2017-04-28 01:03:18 +0300

    implement ProductPicker class which allows to choose arbitrary product from goods list

--- productpicker.cpp
+++ productpicker.cpp
@@ -1,10 +1,140 @@
 #include "productpicker.h"
 #include "ui_productpicker.h"
 
-ProductPicker::ProductPicker(QWidget *parent): QDialog(parent), ui(new Ui::ProductPicker) {
+ProductPicker::ProductPicker(QWidget *parent, QSqlDatabase db_): QDialog(parent), ui(new Ui::ProductPicker) {
     ui->setupUi(this);
+
+    db = db_;
+    is_filled = false;
+    is_searching = false;
+    product_id = -1;
+
+    this->setModal(true);
+    this->setWindowTitle("Перелiк товарiв");
+    this->setFixedSize(this->size());
+
+    refresh();
+    config();
 }
 
 ProductPicker::~ProductPicker() {
     delete ui;
+}
+
+bool ProductPicker::is_valid() { return is_filled; }
+int ProductPicker::pick_product_id() { return product_id; }
+QString ProductPicker::pick_product_name() { return product_name; }
+QString ProductPicker::pick_product_type() { return product_type; }
+
+void ProductPicker::on_buttonBox_accepted(){
+    //qDebug() << ui->table_goods->selectionModel()->currentIndex();
+    int row = ui->table_goods->selectionModel()->currentIndex().row();
+    int col = ui->table_goods->selectionModel()->currentIndex().column();
+
+    if(row < 0 || col < 0) {
+        prompt_error("Ви не обрали товар зi списку! Повторiть спробу ще раз!");
+        return;
+    }
+
+    product_id = ui->table_goods->item(row, 0)->text().toInt();
+    product_name = ui->table_goods->item(row, 1)->text();
+    product_type = ui->table_goods->item(row, 2)->text();
+
+    is_filled = true;
+}
+
+void ProductPicker::on_pushButton_search_clicked() {
+    if(!is_searching && ui->lineEdit_search->text()!="") {
+        QString filter = ui->lineEdit_search->text();
+        QList<QTableWidgetItem *> items = ui->table_goods->findItems(filter, Qt::MatchContains);
+
+        for(int i = 0; i < ui->table_goods->rowCount(); i++)
+            ui->table_goods->hideRow(ui->table_goods->item(i, 0)->row());
+        for(int i = 0; i < items.count(); i++)
+            if(items.at(i)->column()==1) ui->table_goods->showRow(items.at(i)->row());
+
+        ui->pushButton_search->setText("Скасувати");
+        is_searching = true;
+    } else {
+        is_searching = false;
+        ui->lineEdit_search->setText("");
+        ui->pushButton_search->setText("Знайти");
+        show_whole_table();
+    }
+}
+
+void ProductPicker::on_pushButton_addproduct_clicked() {
+    addproduct* form = new addproduct(this, db);
+    while(form->exec()!=QDialog::Rejected && !form->is_valid()) {};
+    delete form;
+
+    refresh();
+    config();
+
+    //go back
+    is_searching = false;
+    ui->lineEdit_search->setText("");
+    ui->pushButton_search->setText("Знайти");
+    show_whole_table();
+}
+
+void ProductPicker::refresh() {
+    QString query = "SELECT * FROM goods_list;";
+    this->fill_table_with_query(ui->table_goods, query);
+    ui->table_goods->refresh();
+    tools::enhance_table_with_cell_tooltip(ui->table_goods);
+}
+
+void ProductPicker::config() {
+    ui->table_goods->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    ui->table_goods->setSelectionMode(QAbstractItemView::SingleSelection);
+    ui->table_goods->setSelectionBehavior(QAbstractItemView::SelectRows);
+
+    for (int i = 0; i < ui->table_goods->horizontalHeader()->count(); ++i)
+        ui->table_goods->horizontalHeader()->setSectionResizeMode(i, QHeaderView::Stretch);
+}
+
+
+void ProductPicker::fill_table_with_query(QTableWidget *tab, QString query){
+    QSqlQuery sql_query = db.exec(query);
+
+    //prompt error message, when sql query failed
+    if(sql_query.lastError().isValid()) {
+        qDebug() << sql_query.lastError();
+        prompt_error("Виникла помилка при завантаженнi данних до таблицi!");
+    }
+
+    //otherwise fill table
+    else {
+        int n = tab->columnCount();
+        tab->setRowCount(sql_query.size());
+        sql_query.first();
+
+        int row = 0;
+        do {
+            for (int i = 0; i < n; i++){
+                QTableWidgetItem* item = new QTableWidgetItem();
+                item->setText(sql_query.value(i).toString());
+                tab->setItem(row, i, item);
+            }
+            row++;
+        } while (sql_query.next());
+
+        tab->resizeColumnsToContents();
+    }
+}
+
+void ProductPicker::show_whole_table() {
+    for(int i = 0; i < ui->table_goods->rowCount(); i++)
+        ui->table_goods->showRow(ui->table_goods->item(i, 0)->row());
+}
+
+void ProductPicker::prompt_error(QString text, bool exit_flag) {
+    QMessageBox messageBox;
+    messageBox.critical(0,"Помилка",text);
+    messageBox.setFixedSize(500,200);
+    if(exit_flag) {
+        QApplication::closeAllWindows();
+        exit(0);
+    }
 }
--- productpicker.h
+++ productpicker.h
@@ -2,6 +2,12 @@
 #define PRODUCTPICKER_H
 
 #include <QDialog>
+#include <QtSql>
+#include <QSqlQueryModel>
+#include <QDesktopWidget>
+#include <QMessageBox>
+#include "addproduct.h"
+#include "tools.h"
 
 namespace Ui {
 class ProductPicker;
@@ -11,11 +17,35 @@ class ProductPicker : public QDialog {
     Q_OBJECT
 
 public:
-    explicit ProductPicker(QWidget *parent = 0);
+    explicit ProductPicker(QWidget *parent, QSqlDatabase db);
     ~ProductPicker();
+
+    bool is_valid();
+    int pick_product_id();
+    QString pick_product_name();
+    QString pick_product_type();
+
+private slots:
+    void on_pushButton_addproduct_clicked();
+    void on_buttonBox_accepted();
+
+    void on_pushButton_search_clicked();
 
 private:
     Ui::ProductPicker *ui;
+    QSqlDatabase db;
+
+    bool is_searching;
+    bool is_filled;
+    int product_id;
+    QString product_name;
+    QString product_type;
+
+    void refresh();
+    void config();
+    void prompt_error(QString text, bool exit_flag = false);
+    void fill_table_with_query(QTableWidget *tab, QString query);
+    void show_whole_table();
 };
 
 #endif // PRODUCTPICKER_H
