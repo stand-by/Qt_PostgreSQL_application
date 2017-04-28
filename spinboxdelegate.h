a67529ad60592b524f927444a5c4da4b4b321f59
eugene <eugene.rublenko@gmail.com>
2017-04-28 17:25:09 +0300

    add DoubleSpinBox delegate

--- spinboxdelegate.h
+++ spinboxdelegate.h
@@ -3,13 +3,28 @@
 
 #include <QObject>
 #include <QSpinBox>
+#include <QDoubleSpinBox>
 #include <QStyledItemDelegate>
 
-class SpinBoxDelegate : public QStyledItemDelegate {
+class SpinBoxDelegate: public QStyledItemDelegate {
     Q_OBJECT
 
 public:
     SpinBoxDelegate(QObject *parent = 0);
+
+    QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
+
+    void setEditorData(QWidget *editor, const QModelIndex &index) const override;
+    void setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const override;
+
+    void updateEditorGeometry(QWidget *editor, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
+};
+
+class DoubleSpinBoxDelegate: public QStyledItemDelegate {
+    Q_OBJECT
+
+public:
+    DoubleSpinBoxDelegate(QObject *parent = 0);
 
     QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
 
